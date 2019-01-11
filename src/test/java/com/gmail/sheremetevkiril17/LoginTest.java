32ea4a0ddd525ad56914c47d935d8574d4168b8b
byamtron <sheremetevkiril17@gmail.com>
2019-01-11 14:09:10 +0300

    Login Successfull
    Login Successfull

--- src/test/java/com/gmail/sheremetevkiril17/LoginTest.java
+++ src/test/java/com/gmail/sheremetevkiril17/LoginTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
@@ -21,24 +23,34 @@ public class LoginTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://panasonic-redaction-qa.oxagile.com");
+        driver.navigate().to("https://panasonic-redaction-qa.oxagile.com");
     }
 
     @Test
-    public void userLogin() {
+    public void userLogin() throws Exception {
         WebElement loginField = driver.findElement(By.id("i0116"));
         loginField.sendKeys("sheremetyevkv@oxagile.com");
         WebElement loginNext = driver.findElement(By.id("idSIButton9"));
         loginNext.click();
         WebElement passwordField = driver.findElement(By.id("i0118"));
         passwordField.sendKeys("Rbhfrtyqwe1723");
-        WebElement signinButton = driver.findElement(By.xpath("//input[@type='submit']"));
-        signinButton.click();
-
+        try {
+            Robot robot = new Robot();
+            robot.mouseMove(1100, 665);
+            robot.delay(500);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.delay(500);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        }
+        catch (AWTException e)
+        {
+            throw new Exception(e);
+        }
 
         /*WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
         String mailUser = profileUser.getText();
         Assert.assertEquals("autotestorgua@ukr.net", mailUser);*/
+
     }
 
 /*    @AfterClass
