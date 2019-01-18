371ea1d03767593db121a8ab0594bc9f1232cd29
byamtron <sheremetevkiril17@gmail.com>
2019-01-18 16:14:53 +0300

    SearchTest
    SearchTest

--- src/test/java/com/gmail/sheremetevkiril17/tests/SearchTest.java
+++ src/test/java/com/gmail/sheremetevkiril17/tests/SearchTest.java
@@ -12,6 +12,7 @@ import org.junit.Assert;
 
 import com.gmail.sheremetevkiril17.pages.LoginAzure;
 
+
 public class SearchTest {
 
     private static WebDriver driver;
@@ -28,7 +29,33 @@ public class SearchTest {
     public void searchCheck() throws Exception {
 
         loginAzure.loginFlow();
-        
+
+        //Проверка на то, что поиск отрабатывает (если есть хоть оидн видос предзагруженный)
+        WebElement buttonSearch = driver.findElement(By.xpath("//button[@kind = 'round']"));
+        buttonSearch.click();
+
+        try{
+            Thread.sleep(3000);
+        }
+        catch(InterruptedException e){
+            throw new Exception(e);
+        }
+
+        if(!driver.findElements(By.cssSelector("div .VideoList__content__Hy8XE")).isEmpty()) {
+            System.out.println("Test Passed for Search check");
+        }
+        else {
+            System.out.println("Test Failed for Search check");
+        }
+
+        //Проверка, что по дефолту сортировка по убыванию
+
+        if(driver.findElements(By.cssSelector("img.VideoList__sortImage__2I0nO.VideoList__sortImageAsc__oNKk3")).isEmpty()) {
+            System.out.println("Test Passed for Sorting");
+        }
+        else {
+            System.out.println("Test Failed for Sorting");
+        }
 
         /*WebElement buttonSearch = driver.findElement(By.xpath("//button[@kind = 'round']"));
         String loginResult = buttonSearch.getText();
