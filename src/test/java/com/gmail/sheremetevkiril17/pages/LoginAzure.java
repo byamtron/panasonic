1982cfcbbc9646960de8aacca207c2a48f0f3568
byamtron <sheremetevkiril17@gmail.com>
2019-01-18 11:48:31 +0300

    LoginAzureTest
    LoginAzureTest

---
+++ src/test/java/com/gmail/sheremetevkiril17/pages/LoginAzure.java
@@ -0,0 +1,54 @@
+package com.gmail.sheremetevkiril17.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.concurrent.TimeUnit;
+
+public class LoginAzure {
+
+    public LoginAzure(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public WebDriver driver;
+
+    @FindBy(id = "i0116")
+    private WebElement loginField;
+
+    @FindBy(id = "idSIButton9")
+    private WebElement loginNext;
+
+    @FindBy(id = "i0118")
+    private WebElement passwordField;
+
+    public void loginFlow() throws Exception {
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to("https://panasonic-redaction-qa.oxagile.com");
+
+        loginField.sendKeys("user1@redadtest1.onmicrosoft.com");
+        loginNext.click();
+        passwordField.sendKeys("Maba6421!");
+
+        try {
+            Robot robot = new Robot();
+            robot.mouseMove(1100, 665);
+            robot.delay(500);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.delay(500);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        }
+        catch (AWTException e)
+        {
+            throw new Exception(e);
+        }
+    }
+}
+
---
+++ src/test/java/com/gmail/sheremetevkiril17/tests/SearchTest.java
@@ -0,0 +1,43 @@
+package com.gmail.sheremetevkiril17.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert;
+
+import com.gmail.sheremetevkiril17.pages.LoginAzure;
+
+public class SearchTest {
+
+    private static WebDriver driver;
+    private static LoginAzure loginAzure;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        loginAzure = new LoginAzure(driver);
+    }
+
+    @Test
+    public void searchCheck() throws Exception {
+
+        loginAzure.loginFlow();
+        
+
+        /*WebElement buttonSearch = driver.findElement(By.xpath("//button[@kind = 'round']"));
+        String loginResult = buttonSearch.getText();
+        Assert.assertEquals("SEARCH", loginResult);*/
+
+    }
+
+    /*@AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }*/
+}
